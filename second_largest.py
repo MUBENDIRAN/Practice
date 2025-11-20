79f1b2168b30322596c4d75881c172977933e274
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-20 09:17:04 +0000

    second largest

--- second_largest.py
+++ second_largest.py
@@ -2,7 +2,7 @@ arr=list(map(int,input().split()))
 largest=arr[0]
 for i in range(1,len(arr)):
     if arr[i] > largest:
-        largest = arr[i]
+        largest=arr[i]
 second=float('-inf')
 for x in arr:
     if x != largest and x > second:
