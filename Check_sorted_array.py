787587e57c9b93771a0778a8e02c4b5cbcef443a
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-20 13:44:10 +0000

    check sorted array

---
+++ Check_sorted_array.py
@@ -0,0 +1,10 @@
+arr=list(map(int,input().split()))
+is_sorted=True
+for i in range(len(arr)-1):
+    if arr[i] > arr[i+1]:
+        is_sorted=False
+        break
+if is_sorted:
+    print('Sorted')
+else:
+    print('Not Sorted')
