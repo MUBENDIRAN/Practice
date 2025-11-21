dd1e18ed0c56f048250369e5eb29102f873c955d
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-21 13:55:49 +0000

    $(1)

--- second_smallest.py
+++ second_smallest.py
@@ -3,7 +3,7 @@ smallest=arr[0]
 for i in range(1,len(arr)):
     if arr[i] < smallest:
         smallest=arr[i]
-# similar to second largest to compare with max largest to get the second smallest we use +infinity a temperory place holder
+# similar to second largest  we use +infinity a temperory place holder
 second=float('inf')
 for x in arr:
     if x != smallest and x < second:
