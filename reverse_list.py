444e88b5dde62c523ced3c7b0e566241eee2960e
root <root@MUBE.>
2025-11-21 10:30:48 +0000

    Added comments

--- reverse_list.py
+++ reverse_list.py
@@ -1,5 +1,5 @@
 arr = list(map(int,input().split()))
-rev = []
-for i in range(len(arr)-1,-1,-1):
-    rev.append(arr[i])
-print(rev)
+rev = [] #for storing the reversed list
+for i in range(len(arr)-1,-1,-1): # start from last goes towards last to first for reversal of list
+    rev.append(arr[i])  # store the reversed list using append since we can add things in list using append 
+print(rev) 
