f174b46f5bf351d519e2a749d5c61ac6994a4fa0
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-22 10:02:38 +0000

    changes

--- missing_number.py
+++ missing_number.py
@@ -1,8 +1,9 @@
+# this problem works when array is 1 to N ,no duplicates ,no extra values than N 
 arr = list(map(int,input().split()))
+# since one number missing which is len -1 so we used len + 1 to calculate 
 N = len(arr) + 1
+# basic maths formula for finding missing number
 expected = N * (N + 1) // 2
 actual = sum(arr)
 missing = expected - actual 
-print (expected)
-print(actual)
 print(missing)
