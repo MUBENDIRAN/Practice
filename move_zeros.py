74cdafb671c4d696ca6e240e7f24a7f9ee8f2722
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-24 10:07:25 +0000

    move zeros

---
+++ move_zeros.py
@@ -0,0 +1,8 @@
+arr = list(map(int,input().split()))
+pos = 0
+for i in range(len(arr)):
+    if arr[i] != 0:
+        arr[pos] = arr[i]
+        pos += 1
+    for i in range(pos,len(arr)):
+        arr[i] = 0
