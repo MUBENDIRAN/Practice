75f476f5b4d3659f26e68e47b71c0a7bd3b21f52
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-24 10:12:14 +0000

    error

--- move_zeros.py
+++ move_zeros.py
@@ -4,6 +4,6 @@ for i in range(len(arr)):
     if arr[i] != 0:
         arr[pos] = arr[i]
         pos += 1
-    for i in range(pos,len(arr)):
+for i in range(pos,len(arr)):
         arr[i] = 0
 print(arr)
