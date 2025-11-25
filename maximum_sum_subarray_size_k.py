0e57769a00b44bdd82e6ec196f797e459e0419da
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-25 10:09:17 +0000

    max sum of subarray size k

---
+++ maximum_sum_subarray_size_k.py
@@ -0,0 +1,9 @@
+arr = list(map(int,input().split()))
+k = int(input())
+window_sum = sum(arr[:k])
+max_sum = window_sum
+for i in range(k,len(arr)):
+    window_sum -= arr[i - k]
+    window_sum += arr[i]
+    max = max(max_sum , window_sum)
+print(max)
