7e92dfbc5b60d9b8811d3e986ce552b38a3b2984
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-25 13:19:32 +0000

    error rectified

--- first_negative_of each_window_of_k.py
+++ first_negative_of each_window_of_k.py
@@ -5,12 +5,11 @@ start = 0
 for end in range(0,len(arr)):
     if arr[end] < 0:
         negatives.append(end)
-    if arr[end] - start + 1 == k:
+    if end - start + 1 == k:
         if negatives:
-            print(negatives([0]))
+            print(arr[negatives[0]])
         else:
             print(0)
         if negatives and negatives[0] == start:
             negatives.pop(0)
         start += 1
-print(negatives)
