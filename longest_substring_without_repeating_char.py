2e59dd10ef45ddf5e5ac0074376307b495ffa9d1
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-26 13:33:04 +0000

    longest substring without repeating char

---
+++ longest_substring_without_repeating_char.py
@@ -0,0 +1,10 @@
+s = input()
+last_seen = {}
+start = 0
+max_len = 0
+for end in range(len(s)):
+    if s[end] in last_seen and last_seen[s[end]] >= start:
+       start = las_seen[s[end]] + 1
+       last_seen[s[end]] = end
+       max_len = max(max_len , end - start + 1)
+       print(max_len)
