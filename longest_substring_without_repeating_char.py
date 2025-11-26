6f5a659e9694cc1e7c5b7f3b89db3a33b885dffc
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-26 13:53:32 +0000

    error

--- longest_substring_without_repeating_char.py
+++ longest_substring_without_repeating_char.py
@@ -4,7 +4,7 @@ start = 0
 max_len = 0
 for end in range(len(s)):
     if s[end] in last_seen and last_seen[s[end]] >= start:
-       start = las_seen[s[end]] + 1
-       last_seen[s[end]] = end
-       max_len = max(max_len , end - start + 1)
-       print(max_len)
+       start = last_seen[s[end]] + 1
+    last_seen[s[end]] = end
+    max_len = max(max_len , end - start + 1)
+print(max_len)
