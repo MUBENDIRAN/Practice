1f925ad21d8d1ed274d5ee79d507461dc2d3f4fe
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-27 12:47:36 +0000

    error

--- binary_tree.py
+++ binary_tree.py
@@ -4,9 +4,9 @@ left = 0
 right = len(arr) - 1
 for i in range(0,len(arr)):
     mid = (left + right) // 2
-    if mid[arr] < num:
+    if mid < num:
         right = mid[arr] + 1
-    elif mid[arr] > num:
+    elif mid > num:
         left = mid[arr] - 1
     else:
         print(found)
