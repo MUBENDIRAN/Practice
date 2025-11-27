6bd16c3f3c8b97c072b52dd7952edae718a5f72f
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-27 14:26:26 +0000

    comment

--- binary_tree.py
+++ binary_tree.py
@@ -1,15 +1,20 @@
 arr = list(map(int,input().split()))
+# number to be searched 
 num = int(input())
 left = 0
 right = len(arr) - 1
 while left < right:
+    # since binary start from middle 
     mid = (left + right) // 2
+    # if middle element is lesser then move the right pointer 
     if arr[mid] < num:
         right = mid + 1
+    # if middle element is higher then move the left pointer
     elif arr[mid] > num:
         left = mid - 1
     else:
         print(mid)
         break
+# this is for safe fail 
 else:
     print(-1)
