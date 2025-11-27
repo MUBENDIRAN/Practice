7d96b949eff7e2d647bd5f4777a2df08e12d59de
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-27 12:50:32 +0000

    error again

--- binary_tree.py
+++ binary_tree.py
@@ -2,13 +2,13 @@ arr = list(map(int,input().split()))
 num = int(input())
 left = 0
 right = len(arr) - 1
-for i in range(0,len(arr)):
+while left < right:
     mid = (left + right) // 2
-    if mid < num:
-        right = mid[arr] + 1
-    elif mid > num:
-        left = mid[arr] - 1
+    if arr[mid] < num:
+        right = arr[mid] + 1
+    elif arr[mid] > num:
+        left = arr[mid] - 1
     else:
-        print(found)
+        print(mid)
         break
 print(-1)
