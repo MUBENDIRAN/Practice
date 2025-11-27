63254b37ffe13491d1726e208999fbd93e3a789e
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-27 12:51:52 +0000

    error fixed

--- binary_tree.py
+++ binary_tree.py
@@ -5,10 +5,11 @@ right = len(arr) - 1
 while left < right:
     mid = (left + right) // 2
     if arr[mid] < num:
-        right = arr[mid] + 1
+        right = mid + 1
     elif arr[mid] > num:
-        left = arr[mid] - 1
+        left = mid - 1
     else:
         print(mid)
         break
-print(-1)
+else:
+    print(-1)
