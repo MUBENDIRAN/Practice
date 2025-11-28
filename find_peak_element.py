949f2cb08915585ce0f22298a51905f1d76d1b81
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-28 10:41:39 +0000

    comments

--- find_peak_element.py
+++ find_peak_element.py
@@ -1,10 +1,13 @@
+# finding the peak means the values is which is greater than neighbouring values it can be more than one
 arr = list(map(int,input().split()))
 left = 0
 right = len(arr) - 1
 while left < right:
     mid = (left + right) // 2
     if arr[mid] < arr[mid + 1]:
+    # If mid element is smaller 
         left = mid + 1
     else:
+    # If mid element is lesser
         right = mid 
 print(left)
