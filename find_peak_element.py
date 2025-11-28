8de934887c291a15d0da07ddae50acf3bf15fd7f
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-28 10:08:00 +0000

    find the peak element

---
+++ find_peak_element.py
@@ -0,0 +1,10 @@
+arr = list(map(int,input().split()))
+left = 0
+right = len(arr) - 1
+while left < right:
+    mid = (left + right) // 2
+    if arr[mid] < arr[mid + 1]:
+        left = mid + 1
+    else:
+        right = mid 
+print(left)
