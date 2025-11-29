fb34d22d2e6cef885184579f8c8cb5f1f3da9ef1
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-29 13:04:48 +0000

    error fixed

--- ceil_value_element.py
+++ ceil_value_element.py
@@ -7,7 +7,7 @@ while left <= right:
     mid = (left + right) // 2
     if arr[mid] >= target:
         ceil_val = arr[mid]
-        left = mid + 1
-    else:
         right = mid - 1
+    else:
+        left = mid + 1
 print(ceil_val)
