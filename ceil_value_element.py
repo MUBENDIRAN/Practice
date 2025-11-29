2b4fc9580e7eef70e4fbfabef6d944f46e4ad991
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-29 13:03:32 +0000

    ceil of an element

---
+++ ceil_value_element.py
@@ -0,0 +1,13 @@
+arr = list(map(int,input().split()))
+target = int(input())
+left = 0
+right = len(arr) - 1
+ceil_val = -1
+while left <= right:
+    mid = (left + right) // 2
+    if arr[mid] >= target:
+        ceil_val = arr[mid]
+        left = mid + 1
+    else:
+        right = mid - 1
+print(ceil_val)
