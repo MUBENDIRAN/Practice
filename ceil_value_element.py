47d823afaf517cb088de60d9679dfb8a9ca61653
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-29 14:01:44 +0000

    comments

--- ceil_value_element.py
+++ ceil_value_element.py
@@ -2,12 +2,15 @@ arr = list(map(int,input().split()))
 target = int(input())
 left = 0
 right = len(arr) - 1
+# similar to floor temp. value before storing the ceil  actual value
 ceil_val = -1
 while left <= right:
     mid = (left + right) // 2
+    # if value is greater than target we can find the ceil of the element by moving towards left to match accurate ceil value
     if arr[mid] >= target:
         ceil_val = arr[mid]
         right = mid - 1
     else:
+    # if the value is smaller than target then move towards the left to find the accurate value 
         left = mid + 1
 print(ceil_val)
