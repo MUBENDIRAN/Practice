5bd1f6371e8b38f65cd80943f5bcd41f9f4794a2
MUBENDIRAN <mubemubendiran@gmail.com>
2025-11-30 07:15:49 +0000

    comments

--- peak_find_unimodal_array.py
+++ peak_find_unimodal_array.py
@@ -3,8 +3,11 @@ left = 0
 right = len(arr) - 1
 while left < right:
     mid = (left + right) // 2
+    # As the mountain peak will be lesser in left and higher in right we use such condition 
     if arr[mid] < arr[mid + 1]:
+        # if condition satisfies we move towards right to find the highest peak
         left = mid + 1
     else:
+        # if not the current mid element is what actually the peak since its a unimodal sorted mountain array
         right = mid 
 print(left)
