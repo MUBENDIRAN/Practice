af16587229b13d7657212f752a5a74bf620ac7dd
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-01 10:39:41 +0000

    comments

--- rotated_sorted_array.py
+++ rotated_sorted_array.py
@@ -7,12 +7,14 @@ while left <= right:
     if arr[mid] == target:
         print(mid)
         break
+    # if the array is sorted and target is in left side 
     elif arr[left] <= arr[mid]:
         if arr[left] <= target < arr[mid]:
             right = mid - 1
         else:
             left = mid + 1
     else:
+    # if the array is sorted and target  is in right 
         if arr[mid] < target <= arr[right]:
             left = mid + 1
         else:
