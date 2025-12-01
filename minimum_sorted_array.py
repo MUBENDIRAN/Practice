4efa188673b86a98759e844bcba2f217b44c0912
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-01 10:42:21 +0000

    comments

--- minimum_sorted_array.py
+++ minimum_sorted_array.py
@@ -3,8 +3,11 @@ left = 0
 right = len(arr) - 1
 while left < right:
     mid = (left + right) // 2
+    # if array is sorted in one side
     if arr[mid] < arr[right]:
+        # move the left pointer if minimum is in right more then mid 
         left = mid + 1
     else:
+        # if not middle is constant in right pointer
         right = mid
 print(arr[left])
