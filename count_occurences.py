663faba2a9c278d80575e9d0f3780e956f6eb785
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-02 08:16:37 +0000

    count occurences

---
+++ count_occurences.py
@@ -0,0 +1,28 @@
+arr = list(map(int,input().split()))
+target = int(input())
+left = 0 
+right = len(arr) - 1
+first = -1
+while left <= right:
+    mid = (left + right) // 2
+    if arr[mid] == target:
+        first = mid
+        right = mid - 1
+    elif arr[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+left = 0
+right = len(arr) - 1
+last = -1
+while left <= right:
+    mid = (left + right) // 2
+    if arr[mid] == target:
+        last = mid
+        left = mid + 1
+    elif arr[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+num = last - first + 1
+print(num) 
