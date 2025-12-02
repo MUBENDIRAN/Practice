e630c313dbbfd75a55d0194590a5ffc0423d955f
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-02 09:59:54 +0000

    comments

--- count_occurences.py
+++ count_occurences.py
@@ -2,9 +2,11 @@ arr = list(map(int,input().split()))
 target = int(input())
 left = 0 
 right = len(arr) - 1
+# like first occurence we used a temp. value before actual
 first = -1
 while left <= right:
     mid = (left + right) // 2
+    # if target found in mid itself just change first and go through left to find the actual first
     if arr[mid] == target:
         first = mid
         right = mid - 1
@@ -14,9 +16,11 @@ while left <= right:
         right = mid - 1
 left = 0
 right = len(arr) - 1  
+# like last occurence we used a temp. value before actual
 last = -1
 while left <= right:
     mid = (left + right) // 2
+    # if target found in the mid itself just change the last and go through the right to find the actual last
     if arr[mid] == target:
         last = mid
         left = mid + 1
@@ -24,5 +28,6 @@ while left <= right:
         left = mid + 1
     else:
         right = mid - 1
+# this formula helps to find the occurence like we use in sliding window "end - start + 1"
 num = last - first + 1
 print(num) 
