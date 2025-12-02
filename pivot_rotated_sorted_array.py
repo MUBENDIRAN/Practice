2813af15aa79cf5c397c1d6c70dc4b3de7112257
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-02 05:48:19 +0000

    comments

--- pivot_rotated_sorted_array.py
+++ pivot_rotated_sorted_array.py
@@ -3,8 +3,10 @@ left = 0
 right = len(arr) - 1
 while left < right:
     mid = (left + right) // 2
+    # if pivot element is in left move the right pointer
     if arr[mid] > arr[right]:
         left = mid + 1
     else:
+    # if the pivot element is in right then right pointer be constant and move only left pointer
         right = mid
 print(left)
