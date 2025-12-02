74c93ef9375d9219a8e5f24797f56b42f3acf125
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-02 10:13:08 +0000

    comments

--- prefix_sum_array.py
+++ prefix_sum_array.py
@@ -1,8 +1,11 @@
-n = int(input())
 arr = list(map(int,input().split()))
+# to store the prefix sumed value one by one during loop
 prefix = []
+# store the live iterated value
 max_sum = 0
 for x in arr:
+    # it sums with the previous(prefix) value 
     max_sum += x
+    # it appended and modifies untill loop runs
     prefix.append(max_sum)
 print(max_sum)
