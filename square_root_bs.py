0b255007891399346e8c13f2c859984abe9b19c0
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-02 10:09:36 +0000

    comments

--- square_root_bs.py
+++ square_root_bs.py
@@ -1,15 +1,22 @@
-n = int(input())
+n = int(input()) 
+# since we gonna calculate it for integer not an array pointer start with one not zero 
 left = 1
+# since the length can be any we used right the last pointer as n for its length
 right = n
+# we used a temp. value before actual 
 ans = 0
 while left <= right:
     mid = (left + right) // 2
+    # It searches the mid index and compare with the given integer, if it matches its perfect square so loop ends
     if mid*mid == n:
         ans = mid
         break
+    # if value lesser move towards right
     elif mid*mid < n:
+        # its has the possible square before moving 
         ans = mid
         left = mid + 1
     else:
+        # if the value is higher move towards left
         right = mid - 1
 print(ans)
