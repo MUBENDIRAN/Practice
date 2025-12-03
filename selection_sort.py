337d54b73350c9a8a8cceb8006cf435d88d2a2d4
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-03 09:09:17 +0000

    error

--- selection_sort.py
+++ selection_sort.py
@@ -2,7 +2,7 @@ arr = list(map(int,input().split()))
 for i in range(len(arr)):
     min_val = i
     for j in range(i+1,len(arr)):
-        if arr[i] < arr[min_val]:
-            min_val = arr[i]
-            arr[i],arr[min_val] = arr[min_val],arr[i]
+        if arr[j] < arr[min_val]:
+            min_val = j
+    arr[i],arr[min_val] = arr[min_val],arr[i]
 print(arr)
