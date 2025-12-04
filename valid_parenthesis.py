d99f93281a756926c7ebe8e9b4f98f3b7284e945
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-04 12:17:40 +0000

    comments

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -1,14 +1,19 @@
 s = input()
+# to store the pairs and check 
 stack = []
 pairs = { ')' : '(', ']' : '[', '}' : '{'}
 for ch in s:
+    # if iterated element is first parenthesis its stored to stack list
     if ch in "([{":
         stack.append(ch)
+#if not close paranthesis its get checked whether its not in stack or not equal to last element in pairs key
+# if something stay like this its not complete paranthesis else its complete we pop and remove that from stack 
     elif ch in ")]}":
         if not  stack or stack[-1] != pairs[ch]:
             print("False")
             break
         else:
             stack.pop()
+    # else the given char is not valid so stack list is empty or zero 
     else:
         print(len(stack) == 0)
