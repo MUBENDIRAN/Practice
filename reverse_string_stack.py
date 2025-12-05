92e480b618beb729caaca8049bc598c49dd74d63
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-05 06:31:59 +0000

    comments

--- reverse_string_stack.py
+++ reverse_string_stack.py
@@ -1,8 +1,12 @@
 s = input()
+# to store the elements in LIFO
 stack = []
 for ch in s:
+    #each element is iterated and added to stack
     stack.append(ch)
+# to store the reverse charater
 rev = ""
+# when stack has elements its get poped from last and stored in rev so we get reverse of the element
 while stack:
     rev += stack.pop()
 print(rev)
