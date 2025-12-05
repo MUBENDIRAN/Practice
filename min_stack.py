f5f21d1744582ee7be7d16bcc3f7bdcdd0037d45
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-05 10:56:47 +0000

    error

--- min_stack.py
+++ min_stack.py
@@ -1,14 +1,15 @@
+arr = list(map(int,input().split()))
 class Minstack:
-    def __init__(self):
+    def __init__(self,arr):
         self.st = []
         self.min_st = []
 
     def push(self,x):
         self.st.append(x)
-        if not min_st:
+        if not self.min_st:
             self.min_st.append(x)
         else:
-            self.min_st(min(x,self.min_st[-1]))
+            self.min_st.append(min(x,self.min_st[-1]))
 
     def pop(self):
         self.st.pop()
