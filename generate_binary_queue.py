c0d64847a51d1e726370cfd6abb54a6eb5f7230c
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-08 14:08:55 +0000

    comments

--- generate_binary_queue.py
+++ generate_binary_queue.py
@@ -1,10 +1,14 @@
+# we use deque from library so we can use queue in O(1)
 from collections import deque
 n = int(input())
 def generateBinary(n):
+    # Asign q to deque so we can use q for deque operations
     q = deque()
+    # since we generate binary from 1 to N the append the initial value 1
     q.append("1")
-
+    # since index value is not important we use _ (underscore)
     for _ in range(n):
+        # we store the first binary element to add with its child element to make a series of continuous binary of N like DFS 
         curr = q.popleft()
         print(curr)
         q.append(curr + "0")
