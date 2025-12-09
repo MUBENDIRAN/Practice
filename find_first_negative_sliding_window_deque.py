c4853c934aeae12be22a40639836cb39bf67fb1e
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-09 13:48:44 +0000

    comments

--- find_first_negative_sliding_window_deque.py
+++ find_first_negative_sliding_window_deque.py
@@ -2,14 +2,19 @@ from collections import deque
 arr = list(map(int,input().split()))
 k = int(input().strip())
 def firstNegative(arr,k):
+    # it stores the indices of negatives
     dq = deque()
+    # it stores the negative value
     res = []
 
     for i in range(len(arr)):
+        # if the first element is left the window its get removed from the list
         if dq and dq[0] < i - k + 1:
             dq.popleft()
+        # if the current element is negative its indices it appended to dq
         if arr[i] < 0:
             dq.append(i)
+        # if the window is full the negatives value is appended to res 
         if i >= k - 1:
             if dq:
                 res.append(arr[dq[0]])
--- max_sliding_window.py
+++ max_sliding_window.py
@@ -1,19 +1,24 @@
 from collections import deque
+# arr for list of numbers and k for windows size
 arr = list(map(int,input().split()))
 k = int(input().strip())
 def maxSlidingWindow(arr,k):
+    # should mention deque to a variable so it becomes deque and can be used for LIFO
+    # this stores the indices of the max value
     dq = deque()
+    # to store the max value 
     res = []
 
     for i in range(len(arr)):
+        # if the first element left the window its get removed from window
         if dq and dq[0] < i - k + 1:
             dq.popleft()
-
+    # if the current element is greater than previously stored element then remove the previously stored element
         while dq and arr[i] >= arr[dq[-1]]:
             dq.pop()
-
+    # after the first element is removed from the window new element is added
         dq.append(i)
-
+    # if the window is full add the first element which is max 
         if i >= k - 1:
             res.append(arr[dq[0]])
     return res
