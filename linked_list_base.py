90b8bef954d7cc9ed0e2e73aef0206c7013f94c1
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-10 10:23:33 +0000

    insert a new node at the bigining of  singly linked list this is a linked list basic

---
+++ linked_list_base.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.head = None
+
+    def insert_at_begining(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
