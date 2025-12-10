e3fb9a9257037ee2031a006217fbbeccee557ff8
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-10 10:30:21 +0000

    insert the node at the end of singly linked list

--- linked_list_base.py
+++ linked_list_base.py
@@ -7,3 +7,13 @@ class Node:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
+    
+    def insert_at_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr = self.head
+        while curr.next is not None:
+            curr = curr.next
+        curr.next = new_node
