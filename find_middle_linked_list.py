2978841064fe614a94c3ad9490d0f8bc0d1ad13c
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-11 09:19:38 +0000

    find the middle in linked list

---
+++ find_middle_linked_list.py
@@ -0,0 +1,12 @@
+def middlelist(self):
+    if self.head is None:
+        return None
+
+    slow = self.head
+    fast = self.head
+
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
