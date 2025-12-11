f6dee7e0460c3fb579c594bc52ef0f9b4e38eb21
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-11 11:36:03 +0000

    comments

--- find_middle_linked_list.py
+++ find_middle_linked_list.py
@@ -1,12 +1,15 @@
+# the linked list basis must be implemented as per need for proper function of this program
 def middlelist(self):
+    # if the head is none no middle element so none
     if self.head is None:
         return None
-
+    # we use floyd two pointer both made initial to head for starting 
     slow = self.head
     fast = self.head
-
+    # if fast and fast.next has value then loop continues
     while fast is not None and fast.next is not None:
+        # slow means goes slow 1/2 speed of fast and fast is to move fast and find the reach
         slow = slow.next
         fast = fast.next.next
-
+    # if fast reached the end the slow would be in the middle so return slow for middle value
     return slow
