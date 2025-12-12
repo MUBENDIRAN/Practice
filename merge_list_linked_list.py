998752c91651cdc27fb4490dc34862c56eb9ddfb
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-12 06:22:44 +0000

    comments

--- merge_list_linked_list.py
+++ merge_list_linked_list.py
@@ -1,7 +1,9 @@
+# add the necessary linked list base to make the complete program 
 def merge_list(self,head1,head2):
+    # to make the logic simple we create a simple dummy variable as the begining and tail will follow up in back starts from it to monitor
     dummy = Node(0)
     tail = dummy 
-
+    # when head1 and 2 not None they are compared and the least one added to tail.next by the way we add from the end and build the merge list
     while head1 is not None and head2 is not None:
         if head1 < head2:
             tail.next = head1
@@ -9,9 +11,9 @@ def merge_list(self,head1,head2):
         else:
             tail.next = head2
             head2 = head2.next
-
+    # since the loop finished only one element will be left so we make tail as tail.next 
     tail = tail.next
-
+    # to assume which head has the last element by None condition and fill the last tail.next and return dummy.next where actual merge list start 
     if head1 is not None:
         tail.next = head1
     else:
