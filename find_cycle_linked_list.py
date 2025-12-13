8c6d2136eaa6c7ee5a6cc98e62c3f34d8ea9aa77
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-13 10:13:49 +0000

    comments

--- find_cycle_linked_list.py
+++ find_cycle_linked_list.py
@@ -1,8 +1,9 @@
-
+# this class is used to use object by different independent function
 class Node:
     def __init__(self,data):
         self.data = data
         self.next = None 
+# this function make the list to linked list
 def build_list(arr):
     head = Node(arr[0])
     curr = head
@@ -10,6 +11,7 @@ def build_list(arr):
         curr.next = Node(x)
         curr = curr.next
     return head
+# this function find the cycle of the linked list
 def cycle_has(head):
     # if  head and head.next is none how it can be cycle , may be self looped but its advanced not for us 
     if head is  None or head.next is None:
@@ -28,6 +30,7 @@ def cycle_has(head):
     return False
 
 arr = list(map(int,input().split()))
-
+# this maintains the original head after the loop finishes 
 head = build_list(arr)
+# since we use only boolean we dont need separate print function so we used print the cycle_has function
 print(cycle_has(head))
