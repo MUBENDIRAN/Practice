e843c1e239ba875adac6f7652ee748ca2bd35ee4
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-13 14:32:01 +0000

    comments + i/o operations added

--- reverse_linkedlist.py
+++ reverse_linkedlist.py
@@ -1,9 +1,22 @@
-# before this function need to asign the linked list fundamentals so nodes to make working reverse linked list
-def reverselinkedlist(self):
+# this class helps in making the arr elements as linked list objects 
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+# this makes the element map to linked list object
+def built(arr):
+    head = Node(arr[0])
+    curr = head
+    for x in arr[1:]:
+        curr.next = Node(x)
+        curr = curr.next
+    return head
+# this reverse the linked list
+def reverselinkedlist(head):
     # initially the current is self.head and others are None for temp. value
-    curr = self.head
+    curr = head
     prev = None
-    curr.next = None
+    
     # if current is not none the loop goes through the nodes one by one
     while curr is not None:
         # this step ensures the node chain does not break by saving the current next values for future current 
@@ -15,4 +28,17 @@ def reverselinkedlist(self):
         # previously stored node is made as current node and the process continue
         curr = next_node
     # this ensure the previous value is the head of other nodes so the chance of error and improper placement of nodes will not happen
-    self.head = prev
+    return prev
+# this function transverse the linked list and print transversely
+def print_list(head):
+    curr = head
+    while curr:
+        print(curr.data,end=" ")
+        curr = curr.next
+    print()
+
+arr = list(map(int,input().split()))
+head = built(arr)
+
+ans = reverselinkedlist(head)
+print_list(ans)
