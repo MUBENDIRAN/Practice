f10533ff4e4020cc2b0e617bf045bd8d496598d2
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-13 08:58:52 +0000

    error

--- find_nth_element_from_end_linked_list.py
+++ find_nth_element_from_end_linked_list.py
@@ -1,10 +1,27 @@
-def remove_nth_element_from_end(self,n):
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+def build_list(arr):
+    if not arr:
+        return None
+    head = Node(arr[0])
+    curr = head
+    for x in arr[1:0]:
+        curr.next = arr(x)
+        curr = curr.next
+    return head
+
+def remove_nth_element_from_end(head,n):
     dummy = Node(0)
-    dummy.next = self.head
+    dummy.next = head
     slow = dummy
     fast = dummy
 
     for _ in range(n):
+        if not fast.next:
+            return dummy.next
         fast = fast.next
 
     while fast is not None:
@@ -14,3 +31,17 @@ def remove_nth_element_from_end(self,n):
     slow.next = slow.next.next
 
     return dummy.next
+
+def print_list(self):
+    curr = head
+    while curr:
+        print(curr.data, end=" ")
+        curr = curr.next
+    print()
+
+arr = list(map(int,input().split()))
+n = int(input())
+
+head = build_list(arr)
+head = remove_nth_element_from_end(head,n)
+print_list(self)
