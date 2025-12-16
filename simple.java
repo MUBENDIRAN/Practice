e8833dc2d3bf92e4b9ee345cb1aea7b23df7d436
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-16 04:43:30 +0000

    simple java if else execution

---
+++ simple.java
@@ -0,0 +1,11 @@
+class simple{
+    public static void main(String[] args){
+        boolean b = false;
+        if (b){
+        System.out.println("if block");
+    }
+    else{
+    System.out.println("else Block");
+    }
+    }
+}
