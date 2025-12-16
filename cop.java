bfb214866514fe41e4635611596b81e92e23bd23
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-16 04:27:31 +0000

    copying the array element using arraycopy and inbuilt array function to print array but its optional

---
+++ cop.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class cop{
+    public static void main(String[] args){
+        int [] a = {1,2,3,4,5};
+        int [] b = new int[a.length];
+        System.arraycopy(a,0,b,0,a.length);
+        System.out.println(Arrays. toString(b));
+    }
+}
--- far.java
+++ far.java
@@ -1,5 +1,6 @@
 class far{
     public static void main(String[] args){
+        // if condition is false no output as condition is false 
         while (true)
         System.out.println("whiile loop");
     }
