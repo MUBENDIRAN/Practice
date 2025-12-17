ddc558b9e16f6fd4dfad8b452a946c97d4b38add
root <root@MUBE.>
2025-12-17 03:52:41 +0000

    c program basics

---
+++ first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	if (0) { printf("First statement"); }
+	if (-10) { printf("Second"); }
+	if (80 - 10 * 8) { printf("third"); }
+	return 0;
+}
---
+++ rit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int x = 40, y = 30, z = 80;
+	if (x < y < z)
+		printf("REC",/n);
+	else
+		printf("RIT",/n);
+	return 0;
+}
