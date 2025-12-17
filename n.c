a09c2b0e1c64a83a5a37b009b8bec16f0768ee23
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-17 04:36:36 +0000

    c basic program output guess practice

---
+++ n.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a =0,b=1,c=-1;
+	if(a)
+		printf("rec\n");
+	if(b)
+		printf("rit\n");
+	if(c)
+		printf("rsa\n");
+	return 0;
+}
---
+++ pro.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int i;
+	if (i=0,2,3)
+		printf("rec\n");
+	else
+		printf("program logic");
+	printf("%d\n",i);
+		return 0;
+}
