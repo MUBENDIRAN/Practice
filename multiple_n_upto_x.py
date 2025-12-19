0f9fce9c67fbd0e3cc556dce47f8380d49dcb1a3
MUBENDIRAN <mubemubendiran@gmail.com>
2025-12-19 04:38:08 +0000

    mutiple of n upto divisible by x

--- multiple_n_upto_x.py
+++ multiple_n_upto_x.py
@@ -1,8 +1,8 @@
 n = int(input())
 x = int(input())
-a = n
-b = x
-while b != 0:
-    a, b = b, a%b
-gcd = a
-if 
+mutiple = n
+while True:
+    print(mutiple)
+    if mutiple%x == 0:
+        break
+    mutiple+=n
