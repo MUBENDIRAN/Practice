2cef39dd6d904153d46e827390735dece3f6d9e5
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-09 15:59:40 +0530

    comments

--- diameter_tree.py
+++ diameter_tree.py
@@ -1,31 +1,34 @@
+# this class defines the structure of the tree like child and values 
 class tree:
     def __init__(self,val):
         self.val = val
         self.left = None
         self.right = None
-
+# this function ensures the diameter of the tree
 def diameter(root):
     max_dia  = 0
-
+# this function ensures the height of the tree with then moved to diameter
     def height(node):
+        # this keyword says to use the variable of diameter
         nonlocal max_dia
         if node is None:
             return -1
-
+    # this addes the nodes of left and right 
         left = height(node.left)
         right = height(node.right)
-
+    # this replace the values as the height of tree updates since diameter is left + right we use plus two 
         max_dia = max(max_dia,left+right+2)
+        # this updates the height of the tree of current node 
         return 1 + max(left,right)
-
+    # function returned untill none and finally dia is returned to dia function
     height(root)
     return max_dia
-
-
+# this defines the values of the root and child
 root = tree(10)
 root.left = tree(5)
 root.right = tree(15)
 root.right.right = tree(20)
+# function is called for output 
 d = diameter(root)
 
 print("Diameter :",d)
--- identical_tree.py
+++ identical_tree.py
@@ -1,29 +1,31 @@
+# this class defines the structure of tree like child and values 
 class tree:
     def __init__(self,val):
         self.val = val
         self.left = None
         self.right = None
-
+# this function ensures the identical of root1 and root2 
 def is_identical(root1,root2):
+    # if both are none with no expections its true 
     if root1 is None and root2 is None:
         return True
-
+    # any one is none before another its not 
     if root1 is None or root2 is None:
         return False
-
+    # values of root also plays a role so its also checked 
     if root1.val != root2.val:
         return False
-
+    # and similarly childs values and strucutre are also checked 
     return(is_identical(root1.left,root2.left)and
     is_identical(root1.right,root2.right))
-
+# values are defined 
 root1 = tree(10)
 root1.left = tree(5)
 root1.right = tree(15)
 root2 = tree(10)
 root2.left = tree(5)
 root2.right = tree(20)
-
+# function called 
 i = is_identical(root1,root2)
 print("identical = ",i)
 
