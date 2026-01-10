65bb82c639bb7c4c20802c363532cf18736615d4
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-10 16:52:16 +0530

    comments

--- lca_bst.py
+++ lca_bst.py
@@ -1,9 +1,10 @@
+# this class defines the structure of the tree like child and value 
 class tree:
     def __init__(self,val):
         self.val = val
         self.left =  None
         self.right = None
-
+# this function finds the lowest common ancestor of the given values p and q 
 def lca(root,p,q):
     if root is None:
         return None
@@ -14,13 +15,15 @@ def lca(root,p,q):
         return lca(root.right,p,q)
     else:
         return root
-
+# the values of root and child is created and constructor is runned 
 root = tree(20)
 root.left = tree(10)
 root.left.left = tree(5)
 root.left.right = tree(10)
 root.right = tree(30)
-p= 5
+#p and q values are defined 
+p = 5
 q = 10
+# the function lca is runned and assigned to l and the value is printed 
 l = lca(root,p,q)
 print("Lowest commmon ancestor of",p,"and",q,"is :",l.val)
--- validate_bst.py
+++ validate_bst.py
@@ -1,10 +1,12 @@
+# this class defines the structure of the tree like child and value 
 class tree:
     def __init__(self,val):
         self.val = val
         self.left = None
         self.right = None
-
+# this checks the binary tree is valid or not we can use inorder too & this follow bst rule left < root < right 
 def valid_bst(root):
+    # this nested function is not necessary we can directly defines in valid_bst but we need to defines the inf and expose it public so we used nested 
     def help(node,low,high):
         if node is None:
             return True
@@ -15,10 +17,10 @@ def valid_bst(root):
         return (help(node.left,low,node.val) and help(node.right,node.val,high))
 
     return help(root,float('-inf'),float('inf'))
-
+# values of root and child is created and constructor is runned when created 
 root = tree(10)
 root.left = tree(50)
 root.right = tree(20)
-
+# the valid bst function is runned 
 v = valid_bst(root)
 print("Is this bst is valid :",v)
