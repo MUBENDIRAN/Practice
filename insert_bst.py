8d6d04c5107fa2d0d176d0d31353c0fa790c075c
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-10 16:43:42 +0530

    comments

--- insert_bst.py
+++ insert_bst.py
@@ -1,9 +1,10 @@
+# this class defines the structure of tree like child and value 
 class tree:
     def __init__(self,val):
         self.val = val
         self.left = None
         self.right = None
-
+# this function enables the inserting of nodes at a perfect place & this also follow bst rule left < root < right 
 def insert(root,val):
     if root is None:
         return tree(val)
@@ -14,22 +15,23 @@ def insert(root,val):
         root.right = insert(root.right,val)
 
     return root
-
+# this function is used to get the tree after the insertion but its sorted you can try some other too 
 def inorder(root):
     if root is None:
         return 
     inorder(root.left)
     print(root.val,end=" ")
     inorder(root.right)
-
+# values of root and child are created and constructor runs when created 
 root = tree(8)
 root.left = tree(3)
 root.right = tree(10)
 root.right.right = tree(14)
-
+# value to be inserted
 val = 13
-
+# this runs the insert function 
 insert(root,val)
 print("final path :",end=" ")
+# this runs the inorder function 
 inorder(root)
 print()
--- search_bst.py
+++ search_bst.py
@@ -1,24 +1,26 @@
+# this class defines the structure of tree like child and value
 class tree:
     def __init__(self,val):
         self.val = val
         self.left = None
         self.right = None
-
+# this function defines the rule of bst left < root < right and search a specific element 
 def search(root,x):
     if root is None:
         return False
-
     if root.val == x:
         return True
     elif x < root.val:
         return search(root.left,x)
     else:
         return search(root.right,x)
-
+# values of root and child are created and constructor created and assign the value 
 root = tree(10)
 root.left = tree(8)
 root.left.left = tree(5)
 root.right = tree(25)
+# search element is defined 
 x= 1
+# function called and assigned to f variable 
 f = search(root,x)
 print("the x is found :",f)
