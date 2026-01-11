3c8d6fca039201262e2e140df81b1fd45c83ed60
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-11 18:40:47 +0530

    sprial print matrix

---
+++ sprial_print.py
@@ -0,0 +1,37 @@
+def sprial_print(matrix):
+    if matrix is None:
+        return []
+
+    result = []
+
+    top = 0
+    bottom = len(matrix) - 1
+    left  = 0
+    right = len(matrix[0]) - 1
+
+    while top <= bottom and left <= right:
+        for col in range(left,right+1):
+            result.append(matrix[top][col])
+        top += 1
+
+        for row in range(top,bottom + 1):
+            result.append(matrix[row][right])
+        right -= 1
+
+        if top <= bottom:
+            for col in range(right,left - 1,-1):
+                result.append(matrix[bottom][col])
+            bottom -= 1
+
+        if left <= right:
+            for row in range(bottom,top - 1,-1):
+                result.append(matrix[row][left])
+            left += 1
+
+    return result 
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+print(sprial_print(matrix))
+    
+
