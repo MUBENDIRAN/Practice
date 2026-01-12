0a98964a93fc9c40d85cd4c5387d0d7dba6abb58
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-12 16:40:28 +0530

    comments

--- rotate_90_matrix.py
+++ rotate_90_matrix.py
@@ -1,27 +1,30 @@
+# this function rotate the matrix in place to 90 degrees 
 def matrix90(matrix):
     n = len(matrix)
-
-    for  i in range(n):
-        for j in range(i,n):
+    # this nested loop transpose the matrix the first right triangle 
+    for  i in range(n): # this holds the row to be transposed 
+        for j in range(i,n): # this transpose the elements
             matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
-
+    # this loop reverse the rows of the matrix so  we get the 90 degree rotated as needed 
     for i in range(n):
-        left , right = 0,len(matrix[i]) - 1
+        left , right = 0,len(matrix[i]) - 1 # we can use reverse function too 
         while left < right:
             matrix[i][left],matrix[i][right] = matrix[i][right],matrix[i][left]
             left += 1
             right -= 1
-
+# we have used manual matrix input for custom input 
 rows = int(input("Enter the number of rows :"))
 column = int(input("Enter the number of column :"))
-
+# we store the matrix values in each rows eqaul to column values so it beomes the square matrix 
 matrix = []
 print("Enter the space separated row values")
 for i in range(rows):
     row = list(map(int,input().split()))
+    # its optional just to handle the error occuring if not square matrix
     if len(row) != column:
         raise ValueError("row must be exactly {} element".format(column))
     matrix.append(row)
+# function called and then matrix printed as rows in space separated     
 matrix90(matrix)
 print("90 degree Rotated matrix")
 for row in matrix:
--- search_sorted_matrix.py
+++ search_sorted_matrix.py
@@ -1,13 +1,14 @@
+# this function searches the target value given in an sorted matrix 
 def search(matrix,target):
     if not matrix:
         return False
-
+    # this has the value of number of rows and columns
     rows = len(matrix)
     column = len(matrix[0])
-
+    # these pointer used to start the comparison from top right corner which is ideal 
     r = 0
     c = column - 1
-
+    # the condition checks the values is lower go left, higher go right 
     while r < rows and c >= 0:
         if matrix[r][c] == target:
             return True
@@ -17,17 +18,18 @@ def search(matrix,target):
             r += 1
     
     return False
-
+# we get values of rows and columns to build the matrix in custom input
 rows = int(input("Enter the number of rows :"))
 column  = int(input("Enter the number of column :"))
-
+# matrix values are stored in individual row values 
 matrix = []
 print("Enter the space separated row values")
 for _ in range(rows):
     row = list(map(int,input().split()))
+    # this is optional just to handle the error if not a square matrix
     if len(row) != column:
         raise ValueError("Values must be equal {} ".format(column))
     matrix.append(row)
-
+# custom input for target as well and function is called to get the output 
 target = int(input("Enter the target value to be searched :"))
 print("the given target value is found :",search(matrix,target))
