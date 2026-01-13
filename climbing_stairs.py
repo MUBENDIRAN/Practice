e323331403577dcae041031c67f10308d08d988c
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-13 15:24:50 +0530

    comments

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -1,17 +1,18 @@
+# this function performs fibonacci similar opertion in space optimised with initial constant value change 
 def climb_stairs(n):
+    # edge case handling
     if n <= 1:
         return n
-
-    prev1 = 1
-    prev2 = 1
-
+    # we use two variable to store constants
+    prev1 = 1 # initial value since if there are 0 ways then only one way exist 
+    prev2 = 1 # follow up value if there is 1 way then forcefully we should go that way so one 
+    # this compute the ways similar to fibonacci 
     for _ in range(2,n+1):
         curr = prev1 + prev2
         prev1 = prev2
         prev2 = curr
 
-    return prev2
-
+    return prev2 # this provide the final output 
+# custom input 
 n = int(input("Enter the number of ways :"))
-
 print(climb_stairs(n))
--- length_lis.py
+++ length_lis.py
@@ -1,16 +1,19 @@
+# this function performs the longest increasing subsequence 
 def length_lis(nums):
+    # to handle edge cases 
     n = len(nums)
     if n  == 0:
         return 0
-
+    # this create a n length of 1 so every element alone is LIS of length one 
     dp = [1]*n
-
+    # the outer loop handles the follow up values and inner loop helps in finding the subsequence with value of i so within range 
     for i in range(n):
         for j in range(i):
+            # this checks whether left is lower and right is higher if then i is the starting point from there sequence starts 
             if nums[j] < nums[i]:
                 dp[i] = max(dp[i] , dp[j] + 1)
 
-    return max(dp)
-
+    return max(dp) # from the list the max is the answer 
+# custom sequence of input 
 nums = list(map(int,input("Enter the sequence :").split()))
 print(length_lis(nums))
