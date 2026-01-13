b63d24a84a79fd8f625c87275ae5fdb0070e5ea2
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-13 15:14:30 +0530

    comments

--- dp_fibo_list.py
+++ dp_fibo_list.py
@@ -1,16 +1,18 @@
+# this function performs fibonacci series with Big O of N space complexity with dynamic programming 
 def dp_fibo(n):
+    # edge case handling
     if n <= 1:
         return n
-
+    # we create empty list to store the series of computed values 
     dp = [0]*(n+1)
-    dp[0] = 0
-    dp[1] = 1
-
+    dp[0] = 0 # initial value 
+    dp[1] = 1 # follow up value 
+    # loop to compute fibonacci values and stored in dp so no recomputing of subproblems 
     for i in range(2,n+1):
         dp[i] = dp[i-1] + dp[i-2]
 
-    return dp[n]
-
-n = int(input("Enter the fibonacci series value :"))
+    return dp[n] # to get final output of final value use n else just dp 
+# custom input 
+n = int(input("Enter the fibonacci series value : "))
 dp  = dp_fibo(n)
 print(dp)
--- dp_fibo_space_optimised.py
+++ dp_fibo_space_optimised.py
@@ -1,17 +1,19 @@
+# this function performs fibonacci series with space optimized using dynamic programming
 def dp_fibo(n):
+    # edge case handling
     if n <= 1:
         return n
-
-    prev1 = 0
-    prev2 = 1
-
+    # constant values to store after each computation 
+    prev1 = 0 # initial value 
+    prev2 = 1 # follow up value 
+    # swaping of computed values happens upto n 
     for _ in range(2,n+1):
         curr = prev1 + prev2
         prev1 = prev2
         prev2 = curr
 
-    return prev2
-
+    return prev2 # to get final output use prev2 if sequence needed use curr
+# custom input 
 n = int(input("Enter the fibonacci series value :"))
 dp = dp_fibo(n)
 print(dp)
