804c86fe38e1f037dc19c21b13a5237865a9ed9e
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-14 21:18:12 +0530

    comments

--- longest_sum_zero.py
+++ longest_sum_zero.py
@@ -1,17 +1,18 @@
+# this function holds the operation of finding the length of longest linear values that makes sum to zero
 def longest_sum_zero(nums):
-    curr = 0
-    first = {0:-1}
-    best = 0
+    curr = 0 # holds the prefix sum value
+    first = {0:-1} # this makes the problem optimized
+    best = 0 # holds the length of longest length 
 
     for i in range(len(nums)):
         x = nums[i]
         curr += x
-        if curr in first:
-            best = max(best,i - first[curr])
+        if curr in first: 
+            best = max(best,i - first[curr]) # if curr found add the initial -1 to i so we get new length 
         else:
-            first[curr] = i
+            first[curr] = i # not found add the value to hashmap
         
     return best
-
+# custom input 
 nums = list(map(int,input("Enter the sequence : ").split()))
 print("Longest sequence of sum  of 0 :",longest_sum_zero(nums))
--- subarray_sum_k.py
+++ subarray_sum_k.py
@@ -1,16 +1,17 @@
+# this function handles the operation of finding the number of pairs have sum of k 
 def subarray_sum_k(nums,k):
-    count = 0
-    curr = 0
-    freq = {0:1}
+    count = 0 # has the count of pairs
+    curr = 0 # holds the prefix sum value
+    freq = {0:1} # this make the problem optimised 
 
     for x in nums:
         curr += x
-        if curr - k in freq:
-            count += freq[curr - k]
-            freq[curr] = freq.get(curr,0) + 1
+        if curr - k in freq: # this is a math formula previous - current = k so previous - k = current 
+            count += freq[curr - k]  # counts the numbber of pairs
+            freq[curr] = freq.get(curr,0) + 1 # adds the curr - k value and curr value 
 
     return count
-
+# custom input 
 nums = list(map(int,input("Enter the sequence : ").split()))
 k = int(input("Enter the value : "))
 
