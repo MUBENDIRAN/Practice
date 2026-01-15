c3b15d2b1787e12958a5eac568eab5bce30534e6
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-15 20:41:19 +0530

    comments

--- insert_interval.py
+++ insert_interval.py
@@ -1,29 +1,30 @@
+# this function holds the operation of inserting the new interval into existing which is sorted 
 def insert_interval(inter,new_inter):
-    res = []
-    i = 0
+    res = [] # stores the final interval after insertion
+    i = 0 
     n = len(inter)
-
+    # if there is no overlapping its already an complete interval so just the interval is appends
     while i < n  and inter[i][1] <= new_inter[0]:
         res.append(inter[i])
         i += 1
-
+    # if there is an overlapping its gets resolved and new interval is generated and appended to res 
     while i < n and inter[i][0] <= new_inter[1]:
         new_inter[0] = min(new_inter[0],inter[i][0])
         new_inter[1] = max(new_inter[1],inter[i][1])
         i += 1
     res.append(new_inter)
-
+    # after the insertion other intervals are added to res to make the final inserted and sorted intervals
     while i < n:
         res.append(inter[i])
         i += 1
 
     return res 
-
+# custom input 
 row = int(input("Enter the number of intervals : "))
 inter = []
 for _ in range(row):
     i = list(map(int,input("Enter the sequence : ").split()))
     inter.append(i)
-
+# custom input
 new_inter = list(map(int,input("Enter the new sequence : ").split()))
 print("Inserted",insert_interval(inter,new_inter))
--- merge_intervals.py
+++ merge_intervals.py
@@ -1,25 +1,28 @@
+# this function holds the opertion of merging the intervals which is unsorted 
 def merge_intervals(inter):
+    # to handle the edge cases
     if len(inter) == 0:
         return []
-
+    # since its unsorted we sort using the first value 
     inter.sort(key = lambda x: x[0])
+    # the first interval is added to merge after sorting 
     merge = [inter[0]]
-
+    # since the first is already merged we start from second so 1 
     i = 1
     while i < len(inter):
-        first = inter[i][0]
-        last = inter[i][1]
-        last_merge = merge[-1][1]
-
+        first = inter[i][0] # first is first value of  interval of second interval
+        last = inter[i][1] # last is next value of  interval of second interval
+        last_merge = merge[-1][1] # last_merge is the last value of overall interval
+    # if there is chance of merging its merged 
         if first <= last_merge:
             merge[-1][1] = max(last_merge,last)
         else:
-            merge.append([first,last])
+            merge.append([first,last]) # if there is no chance of merging append to merge as its already merged
 
         i += 1
 
     return merge
-
+# custom input
 row = int(input("how many intervals : "))
 inter = []
 for _ in range(row):
