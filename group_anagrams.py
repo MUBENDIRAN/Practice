a05e3968f9f2f73dd4d3553de2665f1742a9a781
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-15 15:32:26 +0530

    comments

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,14 +1,15 @@
+# this function holds the operation of finding the number of  group of anagrams found
 def group_anagram(words):
-    mp = {}
+    mp = {} # to store the anagrams with words
 
     for w in words:
-        key = "".join(sorted(w))
+        key = "".join(sorted(w)) # sorted so the problem is simplified 
         if key in mp:
-            mp[key].append(w)
+            mp[key].append(w) # if already there add the new word with that
         else:
-            mp[key] = [w]
+            mp[key] = [w] # else add the new word to list 
 
     return list(mp.values())
-
+# custom input 
 words = input("Enter the words : ").split()
 print("Groups :",group_anagram(words))
--- top_K_elements.py
+++ top_K_elements.py
@@ -1,26 +1,28 @@
+# this function holds the operation of finding the top frequently repeated elements in the list 
 def top_k_element(nums,k):
-    freq = {}
+    freq = {} # this stores the element and its frequency 
     for x in nums:
         if x in freq:
-            freq[x] += 1
+            freq[x] += 1 # this adds the frequency + 1 if already available 
         else:
-            freq[x] = 1
-    bucket = []
-    for _ in range(len(nums) + 1):
+            freq[x] = 1 # if not already add with frequecy with one 
+    
+    bucket = [] # this stores the frequecy and and its number mapped in the descending order 
+    for _ in range(len(nums) + 1): # we have created len + 1 since maintain edge cases 
         bucket.append([])
 
-    for n in freq.items():
-        num = n[0]
-        f = n[1]
-        bucket[f].append(num)
+    for n in freq.items(): # iterate through the frequency 
+        num = n[0] # num stores the value 
+        f = n[1] # f stores the frequency
+        bucket[f].append(num) # frequency is mapped to num value 
 
-    res = []
-    for f in range(len(nums) - 1,0,-1):
-        for num in bucket[f]:
+    res = [] # this stores the top k elements
+    for f in range(len(nums) - 1,0,-1): # we are iterating through backwards since frequency values are descending 
+        for num in bucket[f]: # with the first found frequecy its value is added to res 
             res.append(num)
-            if len(res) == k:
+            if len(res) == k:   # when length of res matches the value of k the result is obtained 
                 return res
-
+# custom input 
 nums = list(map(int,input("Enter the sequenece : ").split()))
 k = int(input("Enter the value : "))
 print("The top elements are :",top_k_element(nums,k))
