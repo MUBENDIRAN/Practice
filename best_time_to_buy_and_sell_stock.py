a980e86bc7bba112a7efb7f2ff3b9db49b17edfc
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-16 14:45:35 +0530

    comments

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -1,15 +1,16 @@
+# this function holds the operation of getting min and max values of the stock and profit earned 
 def buy_sell(prices):
-    min_val = float('inf')
-    max_val = 0
+    min_val = float('inf') # initial edge case handling min value 
+    max_val = 0 # initial edge case handling max value 
 
     for price in prices:
-        if price < min_val:
+        if price < min_val: # if the current stock value is lesser than past new min value is added by replacing the old
             min_val = price
         else:
-            profit = price - min_val
-            max_val = max(max_val,profit)
+            profit = price - min_val # else its gets subtracted and profit value is obtained 
+            max_val = max(max_val,profit) # it is compared with previous max value if its maximum than previous max value is replaced with current
 
     return max_val
-
+# custom input 
 prices = list(map(int,input("Enter the stock values : ").split()))
 print("Profit gained :",buy_sell(prices))
--- two_sum.py
+++ two_sum.py
@@ -1,13 +1,14 @@
+# this function handles operation of best pairs of target to sum
 def two_sum(nums,target):
-    seen = {}
+    seen = {} # holds the unqiue subtracted value for future reference
 
-    for i in range(len(nums)):
+    for i in range(len(nums)): 
         num  = nums[i]
-        need = target - num
-        if need in seen:
+        need = target - num # this creates the unique subtracted value
+        if need in seen: # if the unique subtracted value is already found then current and found is the best two sum pair
             return (seen[need],i)
-        seen[num] = i
-
+        seen[num] = i # if not found its added to seen for future reference
+# custom input
 nums  = list(map(int,input("Enter the sequence : ").split()))
 target = int(input("Enter the target number : "))
 print("The sum indices are :",two_sum(nums,target))
