939e028454177336630ac1958712cedd9fded4f2
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-17 13:59:58 +0530

    comments

--- maximum_contiguous_subarray.py
+++ maximum_contiguous_subarray.py
@@ -1,12 +1,13 @@
+# this function holds the operation of kadanae algorithm of contiguous subarray
 def subarray(nums):
-    curr_max = nums[0]
-    max_sum = nums[0]
-
+    curr_max = nums[0] # initial dummy value
+    max_sum = nums[0] # initial dummy value 
+    # since the first value is already is assigned to curr_max and max_sum to handle edge cases of single value so index start from 1
     for i in range(1,len(nums)):
-        curr_max = max(nums[i],curr_max + nums[i])
-        max_sum = max(max_sum,curr_max)
+        curr_max = max(nums[i],curr_max + nums[i]) # if curr_max is negative then it affects next comming values so ignored and next value act as starting point
+        max_sum = max(max_sum,curr_max) # this holds the latest max value 
 
     return max_sum
-
+# custom input 
 nums = list(map(int,input("Enter the sequence : ").split()))
 print("Max value of contiguous subarray :",subarray(nums))
