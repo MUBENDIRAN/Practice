550cd754f8500458d630775f413e77aabdcd7c89
MUBENDIRAN <mubemubendiran@gmail.com>
2026-01-27 20:57:21 +0530

    added detailed comments and changed the star rhombus pattern to number rhombus pattern by making the iteration i variable as string to another variable a

--- Python/rhombus_pattern.py
+++ Python/rhombus_pattern.py
@@ -1,9 +1,13 @@
+# this function holds the pattern printing of rhombus
 def rhombus(n):
+    # this variable finds the middle element of rhombus width
     mid = n // 2
     for i in range(n):
-        space = abs(mid - i)
-        stars = n - 2*space
-        print(" "*space + "*"*stars)
+        a = str(i) # since we can't concantenate the integer we converted to string 
+        # to perform perfect rhombus we must define perfect space and star since left and right side space determines the perfect rhombus
+        space = abs(mid - i) # we used abs to make the negative value to be absolute
+        stars = n - 2*space # we used this formula since we need to find the perfect spaces in both left and right to place the star for perfect rhombus
+        print(" "*space + a*stars)
 
 n = int(input("Enter the total width of Rhombus :"))
 rhombus(n)
